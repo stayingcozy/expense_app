6c228c23930ab728ab23f48fcc088e5b42335d2d
stayingcozy <cody.colclough@gmail.com>
2022-12-25 09:59:31 -0600

    crismus morn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Groc',
+      amount: 201.12,
+      date: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +36,12 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, // start default
+          crossAxisAlignment: CrossAxisAlignment.center, //center default
           children: <Widget>[
             Container(
-              width: double.infinity,
+              width:
+                  double.infinity, // stretch in Column can accomplish similar
               child: Card(
                 color: Colors.blue,
                 child: Text('CHART!'),
@@ -30,6 +49,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             Card(
+              color: Colors.red,
               child: Text("LIST OF TX"),
             ),
           ],
